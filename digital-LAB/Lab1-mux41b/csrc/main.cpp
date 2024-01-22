297d56afaf49e73d30528bbcc19966e0380b8ef9
Jeanne Willis <lw@mp333player.com>
2024-01-22 19:22:21 +0800

    delete comment out code

--- digital-LAB/Lab1-mux41b/csrc/main.cpp
+++ digital-LAB/Lab1-mux41b/csrc/main.cpp
@@ -1,10 +1,7 @@
 #include <nvboard.h>
 #include <Vtop.h>
 
-// VerilatedContext *contextp = NULL;
-// VerilatedVcdC *tfp = NULL;
 
-// static Vtop *top;
 static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vtop *top);
@@ -23,49 +20,15 @@ static void reset(int n) {
     dut.rst = 0;
 }
 
-// void step_and_dump_wave() {
-//     top->eval();
-//     contextp->timeInc(1);
-//     tfp->dump(contextp->time());
-// }
-
-// void sim_init() {
-//     contextp = new VerilatedContext;
-//     tfp = new VerilatedVcdC;
-//     top = new Vmux41b;
-//     contextp->traceEverOn(true);
-//     top->trace(tfp, 0);
-//     tfp->open("dump.vcd");
-// }
-
-// void sim_exit() {
-//     step_and_dump_wave();
-//     tfp->close();
-// }
-
 int main() {
     nvboard_bind_all_pins(&dut);
     nvboard_init();
-
-    // sim_init();
     
     reset(10);
-
-    // top->a = 0b11100100;
-
-    // top->s = 0b00;
-    // step_and_dump_wave();
-    // top->s = 0b01;
-    // step_and_dump_wave();
-    // top->s = 0b10;
-    // step_and_dump_wave();
-    // top->s = 0b11;
-    // step_and_dump_wave();
 
     while(1){
         nvboard_update();
         single_cycle();
     }
 
-    // sim_exit();
 }
