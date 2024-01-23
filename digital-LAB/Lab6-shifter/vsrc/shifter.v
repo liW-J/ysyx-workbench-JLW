f2300d5bf0d520c759b21f0379612dd14553567b
Jeanne Willis <lw@mp333player.com>
2024-01-23 21:34:29 +0800

    format code

--- digital-LAB/Lab6-shifter/vsrc/shifter.v
+++ digital-LAB/Lab6-shifter/vsrc/shifter.v
@@ -1,11 +1,12 @@
 module shifter(
-    input  clk,
+    input clk,
+    input rst,
     input  wire [7:0] i_num,
     output reg  [7:0] o_num
 );
     parameter CLK_NUM = 4000000;
 
-    reg [31:0] count = 32'h0;
+    reg [31:0] count = 32'b0;
     reg new_bit   = 1'b0;
 
     initial begin
@@ -15,13 +16,11 @@ module shifter(
     assign new_bit = o_num[4] ^ o_num[3] ^ o_num[2] ^ o_num[0];
 
     always @(posedge clk) begin
-        if (count == CLK_NUM) begin
-            count <=32'h0;
-            o_num <= { new_bit, o_num[7:1] };
-        end
-        else begin
-            count <= count + 1;
-        end
+    if (rst) begin o_num <= i_num; count <= 0; end
+    else begin
+      if (count == 0) o_num <= { new_bit, o_num[7:1] };
+      count <= (count >= 5000000 ? 32'b0 : count + 1);
+      end
     end
 
 endmodule
--- digital-LAB/Lab6-shifter/vsrc/top.v
+++ digital-LAB/Lab6-shifter/vsrc/top.v
@@ -1,10 +1,12 @@
 module top(
-    input  clk,
+    input   clk,
+    input   rst,
     output reg  [7:0] ledr
 );
 
     shifter shifter_inst0(
         .clk(clk),
+        .rst(rst),
         .i_num(8'b0000_0001),
         .o_num(ledr));
 
