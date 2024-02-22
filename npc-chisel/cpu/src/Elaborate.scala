866891dfe55da2fbb0d3292740d5a3208f65ffad
Jeanne Willis <lw@mp333player.com>
2024-02-22 11:20:32 +0800

    Delete npc-chisel directory

--- npc-chisel/cpu/src/Elaborate.scala
+++
@@ -1,8 +0,0 @@
-import unit.PCRegister
-import circt.stage._
-
-object Elaborate extends App {
-  def top = new TOP()
-  val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
-}
