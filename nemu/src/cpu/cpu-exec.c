d0bbc66496e47a52b14f4b66e79bad355df6735e
Jeanne Willis <lw@mp333player.com>
2024-03-11 11:22:37 +0800

    finish PA2.2

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -45,7 +45,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (FTRACE_COND) //TODO
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-  // IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   IFDEF(CONFIG_WATCHPOINT, difftest_watchpoint());
 }
 
--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,8 +18,18 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  //TODO
-  return false;
+
+  if (cpu.pc != ref_r->pc) {
+    return false;
+  }
+
+  for (int i = 0; i < 32; i++) {
+    if (gpr(i) != ref_r->gpr[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 void isa_difftest_attach() {
