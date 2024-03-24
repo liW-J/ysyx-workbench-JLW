235ce2b3655a2f503c75838c97e4eb161194ebca
Jeanne Willis <lw@mp333player.com>
2024-03-24 09:42:48 +0800

    finish timer

--- nemu/src/monitor/trace/mtrace.c
+++ nemu/src/monitor/trace/mtrace.c
@@ -2,9 +2,9 @@
 #include <memory/paddr.h>
 
 void display_pread(paddr_t addr, int len){
-  LOG(DEBUG, "pread at " FMT_PADDR ", len=%d.", addr, len);
+  // LOG(DEBUG, "pread at " FMT_PADDR ", len=%d.", addr, len);
 }
 
 void display_pwrite(paddr_t addr, int len,  word_t data){
-  LOG(DEBUG, "pwrite at " FMT_PADDR ", len=%d, data=" FMT_WORD ".", addr, len, data);
+  // LOG(DEBUG, "pwrite at " FMT_PADDR ", len=%d, data=" FMT_WORD ".", addr, len, data);
 }
